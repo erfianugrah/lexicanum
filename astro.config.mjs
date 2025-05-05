c38fcac4d80ab98286e5f163c3d0250072ea9c56
Erfi Anugrah <erfi.anugrah@gmail.com>
2025-05-05 09:49:47 +0000

    init

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import rehypeMermaid from "rehype-mermaid";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://erfi.dev",
+  integrations: [
+    starlight({
+      title: "Tech Docs",
+      social: [{
+        icon: "github",
+        label: "GitHub",
+        href: "https://github.com/withastro/starlight",
+      }],
+      sidebar: [
+        {
+          label: "Guides",
+          autogenerate: { directory: "guides" },
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" },
+        },
+      ],
+    }),
+  ],
+  markdown: {
+    syntaxHighlight: {
+      type: 'shiki',
+      excludeLangs: ['mermaid'],
+    },
+    rehypePlugins: [rehypeMermaid],
+  },
+});
